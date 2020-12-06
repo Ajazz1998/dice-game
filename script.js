0082ac638b95fc60229c10d6d9d70ea739a29c54
Ajay Bava <ajaybava1998@gmail.com>
2020-12-06 15:36:47 +0000

    commit

--- script.js
+++ script.js
@@ -25,19 +25,36 @@ document.querySelector('.btn--roll').addEventListener('click', () => {
         roundScore += dice;
         document.querySelector('#current--' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current--0').textContent = '0';
-        document.getElementById('current--1').textContent = '0';
-
-        document.querySelector('.player--0').classList.toggle('player--active');
-        document.querySelector('.player--1').classList.toggle('player--active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
-document.querySelector('.btn--bold').addEventListener('click', () => {
+document.querySelector('.btn--hold').addEventListener('click', () => {
     scores[activePlayer] += roundScore;
-})
+
+    document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 10){
+        document.querySelector('.player--' + activePlayer).textContent = 'player--winner!';
+        document.querySelector('.dice').style.display = 'none';
+
+        document.querySelector('.player--' + activePlayer).classList.add('player--winner');
+        document.querySelector('.player--' + activePlayer).classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+    
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
+
+    document.querySelector('.player--0').classList.toggle('player--active');
+    document.querySelector('.player--1').classList.toggle('player--active');
+
+    document.querySelector('.dice').style.display = 'none';
+};
